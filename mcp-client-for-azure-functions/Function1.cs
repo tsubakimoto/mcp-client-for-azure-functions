237356efd13b84d8c4f5f3b07d66a2903d302089
Yuta Matsumura <tkmbmbr@gmail.com>
2025-08-10 14:33:38 +0900

    create sample

--- mcp-client-for-azure-functions/Function1.cs
+++ mcp-client-for-azure-functions/Function1.cs
@@ -1,7 +1,13 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
 
 namespace mcp_client_for_azure_functions;
 
@@ -15,9 +21,55 @@ public class Function1
     }
 
     [Function("Function1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+
+        SseClientTransport clientTransport = new(
+            new SseClientTransportOptions
+            {
+                Name = "Microsoft Learn MCP Server",
+                Endpoint = new Uri("https://learn.microsoft.com/api/mcp"),
+                TransportMode = HttpTransportMode.StreamableHttp
+            });
+        var client = await McpClientFactory.CreateAsync(clientTransport);
+
+        var tools = await client.ListToolsAsync();
+        var tool = tools.FirstOrDefault();
+        if (tool is null)
+        {
+            return new BadRequestObjectResult("No tools found in MCP server.");
+        }
+        _logger.LogInformation($"{tool.Name} ({tool.Description})");
+
+        var result = await client.CallToolAsync(
+            tool.Name,
+            new Dictionary<string, object?>
+            {
+                ["query"] = "Please tell me about Azure Functions."
+            });
+
+        foreach (var content in result.Content)
+        {
+            if (content is TextContentBlock tb)
+            {
+                var contents = JsonSerializer.Deserialize<ICollection<MSLearnContent>>(tb.Text);
+                return new OkObjectResult(contents);
+            }
+        }
+
+        return new NotFoundResult();
     }
+}
+
+public class MSLearnContent
+{
+    [JsonPropertyName("title")]
+    public string? Title { get; set; }
+
+    [JsonPropertyName("content")]
+    public string? Content { get; set; }
+
+    [JsonPropertyName("contentUrl")]
+    public string? ContentUrl { get; set; }
 }
